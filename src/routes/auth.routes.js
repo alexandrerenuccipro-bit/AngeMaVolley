2b731280d58d23c8868ce3138de12b905c549717
Alexandre Renucci <alexandre.renucci.pro@gmail.com>
2026-03-30 23:29:01 +0200

    Merge

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,7 +9,10 @@ const router = express.Router();
 
 router.get('/connexion', redirectIfAuthenticated, authController.showLogin);
 router.post('/connexion', redirectIfAuthenticated, authController.login);
+router.get('/inscription', redirectIfAuthenticated, authController.showRegister);
+router.post('/inscription', redirectIfAuthenticated, authController.register);
 router.get('/dashboard', requireAuth, authController.dashboard);
+router.post('/dashboard/demande-licence', requireAuth, authController.requestLicence);
 router.post('/deconnexion', requireAuth, authController.logout);
 
 module.exports = router;
